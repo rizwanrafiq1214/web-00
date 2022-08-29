49f47db0fbac44eb4c209757e887d2c2be7531a9
Magnus Ågren <magnus.agren@chalmers.se>
2022-08-29 15:05:48 +0000

    Deployment fixes for 2022
    Also incremented the course code.

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ app.use(cors());
 
 // Import routes
 app.get('/api', function(req, res) {
-    res.json({'message': 'Welcome to your DIT341 backend ExpressJS project!'});
+    res.json({'message': 'Welcome to your DIT342 backend ExpressJS project!'});
 });
 
 // Catch all non-error handler for api (i.e., 404 Not Found)
