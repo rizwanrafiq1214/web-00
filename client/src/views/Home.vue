8fd07ec94ed69ba495413cbc8d35a3e3ff36f0ad
rizwanra <rizwanra@student.chalmers.se>
2023-10-19 23:36:15 +0200

    my changed code from master

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -29,6 +29,7 @@ export default {
           this.message = error
         })
     }
+
   }
 }
 </script>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,28 @@
+// eslint-disable-next-line
+/* eslint-disable */
+<template>
+<h1>{{message}}</h1>
+</template>
+<script>
+// @ is an alias to /src
+import { Api } from '@/Api'
+export default {
+  name: 'login',
+  data() {
+    return {
+      message: 'none'
+    }
+  },
+  methods: {
+    getMessage() {
+      Api.get('/mytest')
+        .then(response => {
+          this.message = response.data.anothermessage
+        })
+        .catch(error => {
+          this.message = error
+        })
+    }
+  }
+}
+</script>
